b597e3002b1f12249f45dc6dd8800296c9aa6c83
Piyumalsha Isurandi <piyumalshaisurandi@gmail.com>
2026-07-12 00:11:17 +0530

    new commit

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,8 @@ module.exports = function (api) {
 
   return {
     presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }]
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
     ],
-    plugins: [
-      'nativewind/babel'
-    ]
   }
 }
